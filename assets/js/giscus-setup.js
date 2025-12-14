94f60de2266f121b614c81727e53b6a132ef6518
Andrew-Caruso <96966806+Andrew-Caruso@users.noreply.github.com>
2025-12-14 20:09:11 +0000

    Deploying to gh-pages from @ Andrew-Caruso/Andrew-Caruso.github.io@de0369be1d4eb6b31e41e6fae282d15edd76d399 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Andrew-Caruso/Andrew-Caruso",
+    "data-repo": "Andrew-Caruso/Andrew-Caruso.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
